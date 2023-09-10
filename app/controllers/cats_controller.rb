b17ddcbdaf64b116be347d450c99add891d29c84
Yo Kamada <bumpfuji10@gmail.com>
2023-09-10 22:42:21 +0900

    add: indexで複数画像を取得

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -1,6 +1,6 @@
 class CatsController < ApplicationController
   def index
     cat_service = CatApiService.new
-    @cat_image = cat_service.random_cat_image
+    @cat_images = cat_service.multiple_cat_images(10)
   end
 end
