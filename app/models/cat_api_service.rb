cc3fb70cdb02e21598e7313943bb93113c65ba04
Yo Kamada <bumpfuji10@gmail.com>
2023-09-10 22:04:32 +0900

    change: faradayへのリクエストのロジックを初期化煮含める

--- app/models/cat_api_service.rb
+++ app/models/cat_api_service.rb
@@ -1,19 +1,15 @@
 class CatApiService
 
-  def initialize(url="https://api.thecatapi.com/v1/")
-    @url = url
+  def initialize
+    @conn = Faraday.new(url: "https://api.thecatapi.com/v1/") do |conn|
+      conn.request :url_encoded
+      conn.adapter Faraday.default_adapter
+      conn.headers['x-api-key'] = ENV['CAT_API_KEY']
+    end
   end
 
   def random_cat_image
     response = @conn.get('images/search')
     JSON.parse(response.body).first
-  end
-
-  def connection
-    @conn = Faraday.new(url: @url) do |conn|
-      conn.request :url_encoded
-      conn.adapter Faraday.default_adapter
-      conn.headers['x-api-key'] = ENV['CAT_API_KEY']
-    end
   end
 end
